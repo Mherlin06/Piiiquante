1b06c2ffbb72d9285247af38610933f194ef80a5
Hugues Mourice <mourice.hugues@gmail.com>
2022-09-05 18:13:33 +0200

    cors + connect to database

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://Mherlin06:P6-password@projet6.c5rfnv1.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.json({ message: 'la requête a bien été reçue!'})
+app.use('/api/auth/signup', (req, res, next) => {
+    const users = [
+        {
+            email: 'momo@mail.fr' ,
+            password: 'momopass',
+        }
+    ];
+    res.status(200).json(users);
 });
 
 module.exports = app;
+
+// Mherlin06 - P6-password ///
