6c6cf581e9587f06e710a0098e584ffbdb02e3ce
Hugues Mourice <mourice.hugues@gmail.com>
2022-09-06 21:00:17 +0200

    body parser added

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({ path:'./config/.env'});
 
 const userRoutes = require('./routes/user.routes');
@@ -14,12 +15,17 @@ mongoose.connect('mongodb+srv://' + process.env.DB_USER_PASS + '@projet6.c5rfnv1
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch( error => console.log('Connexion à MongoDB échouée !', error ));
 
+// Fix CORS 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+// Parse the data into json format
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/auth', userRoutes);
