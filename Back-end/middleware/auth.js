22e1f967c9a1a1b6cb1d9cdb602803b13358856b
Hugues Mourice <mourice.hugues@gmail.com>
2022-09-08 22:10:05 +0200

    token authentication added

--- Back-end/middleware/auth.js
+++ Back-end/middleware/auth.js
@@ -4,7 +4,10 @@ module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, TOKEN_KEY);
-        const userId= decodedToken.userId;
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
     } catch(error) {
         res.status(401).json({error});
     };
--- Back-end/routes/sauce.routes.js
+++ Back-end/routes/sauce.routes.js
@@ -1,11 +1,12 @@
+const auth = require('../middleware/auth');
 const router = require('express').Router();
 const sauceController = require('../controllers/sauce.controller');
 
-router.post('/', sauceController.createSauce);
-router.get('/', sauceController.getAllSauces);
-router.get('/:id', sauceController.getOneSauce);
-// router.put('/:id', sauceController.modifySauce);
-// router.delete('/:id', sauceController.deleteSauce);
-// router.post('/:id/like', sauceController.setLike);
+router.post('/', auth, sauceController.createSauce);
+router.get('/', auth, sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.getOneSauce);
+// router.put('/:id', auth, sauceController.modifySauce);
+// router.delete('/:id', auth, sauceController.deleteSauce);
+// router.post('/:id/like', auth, sauceController.setLike);
 
 module.exports = router;
