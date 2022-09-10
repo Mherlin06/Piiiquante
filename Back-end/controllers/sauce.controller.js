5f1f8fe205e3f90d72a61c3e3746454489e7e869
Hugues Mourice <mourice.hugues@gmail.com>
2022-09-10 11:22:53 +0200

    updateSauce added

--- Back-end/controllers/sauce.controller.js
+++ Back-end/controllers/sauce.controller.js
@@ -27,7 +27,24 @@ module.exports.readOneSauce = (req, res, next) => {
 };
 
 module.exports.updateSauce = (req, res, next) => {
-
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {
+        ...req.body
+    };
+    delete sauceObject.userId;
+    sauceModel.findOne({_id: req.params.id})
+        .then( sauce => {
+            if (sauce.userId != req.auth.userId){
+                res.status(401).json({ message : 'Modification non autorisée'})
+            } else {
+                sauceModel.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
+                    .then(() => res.status(200).json({ message: "mofidication de la sauce accomplie avec succès" }))
+                    .catch( error => res.status(401).json({ error }));
+            }
+        })
+        .catch(error => res.status(400).json( { error }));
 };
 
 module.exports.deleteSauce = (req, res, next) => {
--- Back-end/routes/sauce.routes.js
+++ Back-end/routes/sauce.routes.js
@@ -6,7 +6,7 @@ const multer = require('../middleware/multer-config');
 router.post('/', auth, multer, sauceController.createSauce);
 router.get('/', auth, sauceController.readAllSauces);
 router.get('/:id', auth, sauceController.readOneSauce);
-router.put('/:id', auth, sauceController.updateSauce);
+router.put('/:id', auth, multer, sauceController.updateSauce);
 router.delete('/:id', auth, sauceController.deleteSauce);
 router.post('/:id/like', auth, sauceController.setLike);
 
