a9892c0c01a1564ee023203a602c150c5d847de1
Hugues Mourice <mourice.hugues@gmail.com>
2022-09-10 11:24:48 +0200

    prettying sauce.controller

--- Back-end/controllers/sauce.controller.js
+++ Back-end/controllers/sauce.controller.js
@@ -1,56 +1,74 @@
-const sauceModel = require('../models/sauce.model');
+const sauceModel = require("../models/sauce.model");
 
 module.exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-    delete sauceObject.userId;
-    const sauce = new sauceModel({
-        ...sauceObject,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: "La sauce a bien été enregistrée !"}))
-      .catch(error => res.status(400).json({error}));
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  delete sauceObject.userId;
+  const sauce = new sauceModel({
+    ...sauceObject,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+  sauce
+    .save()
+    .then(() =>
+      res.status(201).json({ message: "La sauce a bien été enregistrée !" })
+    )
+    .catch((error) => res.status(400).json({ error }));
 };
 
 module.exports.readAllSauces = (req, res, next) => {
-    sauceModel.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({error}));
+  sauceModel
+    .find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 module.exports.readOneSauce = (req, res, next) => {
-    sauceModel.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(400).json({error}));
+  sauceModel
+    .findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(400).json({ error }));
 };
 
 module.exports.updateSauce = (req, res, next) => {
-    const sauceObject = req.file ? {
+  const sauceObject = req.file
+    ? {
         ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {
-        ...req.body
-    };
-    delete sauceObject.userId;
-    sauceModel.findOne({_id: req.params.id})
-        .then( sauce => {
-            if (sauce.userId != req.auth.userId){
-                res.status(401).json({ message : 'Modification non autorisée'})
-            } else {
-                sauceModel.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
-                    .then(() => res.status(200).json({ message: "mofidication de la sauce accomplie avec succès" }))
-                    .catch( error => res.status(401).json({ error }));
-            }
-        })
-        .catch(error => res.status(400).json( { error }));
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : {
+        ...req.body,
+      };
+  delete sauceObject.userId;
+  sauceModel
+    .findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({ message: "Modification non autorisée" });
+      } else {
+        sauceModel
+          .updateOne(
+            { _id: req.params.id },
+            { ...sauceObject, _id: req.params.id }
+          )
+          .then(() =>
+            res
+              .status(200)
+              .json({
+                message: "mofidication de la sauce accomplie avec succès",
+              })
+          )
+          .catch((error) => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => res.status(400).json({ error }));
 };
 
-module.exports.deleteSauce = (req, res, next) => {
+module.exports.deleteSauce = (req, res, next) => {};
 
-};
-
-module.exports.setLike = (req, res, next) => {
-
-};
+module.exports.setLike = (req, res, next) => {};
